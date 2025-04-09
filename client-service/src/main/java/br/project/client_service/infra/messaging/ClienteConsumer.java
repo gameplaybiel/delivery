08062d4640fb183235408eca0178323306b3e440
Gabriel <gabrielconceicao569@gmail.com>
2025-04-09 13:51:32 -0300

    ClienteConsumer

---
+++ client-service/src/main/java/br/project/client_service/infra/messaging/ClienteConsumer.java
@@ -0,0 +1,13 @@
+package br.project.client_service.infra.messaging;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ClienteConsumer {
+
+    @RabbitListener(queues = "{queue.name}")
+    public void consumirMensagem(String message){
+        System.out.println("Mensagem recebida: " + message);
+    }
+}
